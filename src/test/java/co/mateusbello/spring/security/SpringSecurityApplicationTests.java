7190f34488ac27c15bc99df0b093ce343794807a
John Mateus B <jmateus@sambasafety.com>
2024-02-06 17:12:35 -0500

    Initial commit

---
+++ src/test/java/co/mateusbello/spring/security/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package co.mateusbello.spring.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
